70cac378a51d8e27de4fed6ae244993253a95818
castle <castlesmadeofcode@gmail.com>
2020-04-12 22:48:42 -0500

    completed Practice: Planet List

---
+++ planetList.py
@@ -0,0 +1,25 @@
+planet_list = ["Mercury", "Mars"]
+
+# Use append() to add Jupiter and Saturn at the end of the list.
+planet_list.append("Jupiter")
+planet_list.append("Saturn")
+
+# Use the extend() method to add another list of the last two planets in our solar system to the end of the list.
+planets = ["Uranus", "Neptune"]
+planet_list.extend(planets)
+
+# Use insert() to add Earth, and Venus in the correct order.
+planet_list.insert(1,"Venus")
+planet_list.insert(2,"Earth")
+
+# Use append() again to add Pluto to the end of the list.
+planet_list.append("Pluto")
+
+
+# Now that all the planets are in the list, slice the list in order to get the rocky planets into a new list called rocky_planets.
+rocky_planets = planet_list[slice(4)]
+# print(rocky_planets)
+
+# Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the del operation to remove it from the end of planet_list.
+del planet_list[-1]
+print(planet_list)
